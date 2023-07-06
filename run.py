b42cffd2b3ae9f778e0b4317688d57e8831e900d
Froousty <136267022+Froousty@users.noreply.github.com>
2023-07-06 19:45:42 +0200

    Update run.py

--- run.py
+++ run.py
@@ -82,18 +82,16 @@ def ParseSignal(signal: str) -> dict:
     if(len(signal) > 7):
         trade['TP'].append(float(signal[7].split()[-1]))
         
-    # trade['PositionSize'] = float((signal[2].split())[-1])
-    # trade['Multiplier'] = float((signal[3].split())[-1])
     trade['PositionSize'] = float((signal[2].split())[-1])
-    trade['Multiplier'] = (signal[3].split())[-1]
+    trade['Multiplier'] = float((signal[3].split())[-1])
     
-    # logger.info(trade['OrderType'])
-    # logger.info(trade['Symbol'])
-    # logger.info(trade['Entry'])
-    # logger.info(trade['Multiplier'])
-    # logger.info(trade['PositionSize'])
-    # logger.info(trade['StopLoss'])
-    # logger.info(trade['TP'])
+    logger.info(trade['OrderType'])
+    logger.info(trade['Symbol'])
+    logger.info(trade['Entry'])
+    logger.info(trade['Multiplier'])
+    logger.info(trade['PositionSize'])
+    logger.info(trade['StopLoss'])
+    logger.info(trade['TP'])
     
     return trade
     
